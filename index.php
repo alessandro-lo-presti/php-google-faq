fef3296fcad3673ed502e2c22248d7ce4d4269e1
alessandro-lo-presti <mitlopresti@hotmail.it>
2021-02-26 16:19:15 +0100

    header look

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+
+  <body>
+
+    <header>
+
+      <div class="header-top">
+
+        <div class="logo">
+          <img src="img/logo.png" alt="">
+          <a href="#">Privacy e termini</a>
+        </div>
+
+        <div class="account">
+          <i class="fas fa-bars"></i>
+          <span>p</span>
+        </div>
+
+      </div>
+
+      <div class="header-bottom">
+        <ul>
+          <li>
+            <a href="#">
+              Introduzione
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Norme sulla privacy
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Termini di servizio
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Tecnologie
+            </a>
+          </li>
+          <li class="active">
+            <a href="#">
+              Domande Frequenti
+            </a>
+          </li>
+        </ul>
+      </div>
+
+    </header>
+
+    <main>
+
+      <div class="container">
+
+      </div>
+
+    </main>
+
+  </body>
+
+</html>
